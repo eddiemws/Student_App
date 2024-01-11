4957bc992200abf6f3bc23169ccbf050d5a5fb00
eddy <106661971+eddiemws@users.noreply.github.com>
2024-01-11 21:51:55 +0300

    Update grades_page.dart

--- lib/grades.dart/grades_page.dart
+++ lib/grades.dart/grades_page.dart
@@ -1,13 +1,3 @@
-//import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class GradesPage extends StatefulWidget {
-  const GradesPage({Key? key}) : super(key: key);
-
-  @override
-  State<GradesPage> createState() => _MyAppState();
-}
 
 //void initState() {
 // super.initState();
